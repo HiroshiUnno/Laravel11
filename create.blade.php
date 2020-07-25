16839ed108022c8d2d8e19dba0e27926784485aa
”HiroshiUnno” <u.hiroshi19920103@gmail.com>
2020-07-26 00:03:37 +0900

    first commit

---
+++ create.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, inirial-scake=1">
+
+        <title>createblade</title>
+    </head>
+    <body>
+        <h1>CREATE BLADE</h1>
+    </body>
+</html>
